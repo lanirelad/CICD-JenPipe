2dbbd8c5ea8d81ed15b9bfcfaa85a7788df6f1db
lanirelad <lanirelad@gmail.com>
2024-09-01 19:24:50 +0300

    all over

--- provisions/install-docker.sh
+++ provisions/install-docker.sh
@@ -34,4 +34,5 @@ sudo newgrp docker
 
 #---
 # install docker-compose
-sudo apt install docker-compose -y
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
